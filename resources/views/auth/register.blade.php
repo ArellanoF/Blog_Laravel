5eea3f47e8c5b55fbe448d8e150dbe6b63888361
ArellanoF <CORREO@example.comfrancescarellano@gmail.com>
2023-05-26 16:14:41 +0200

    register work

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,38 +2,30 @@
 @section('styles')
 <link rel="stylesheet" href="{{asset('css/login/css/login.css')}}" class="css">
 @endsection
-
 @section('title', 'Crear cuenta')
 @section('content')
-
 <form method="POST" class="form" action="{{route('register')}}" novalidate>
     @csrf
     <h2>Crear cuenta</h2>
     <div class="content-login">
         <div class="input-content">
-            <input type="text" name="full_name" placeholder="Nombre completo"
-                value="{{old('full_name')}}" 
-                autofocus>
-
-                @error('full_name')
+        <input id="name" type="text" placeholder="Nombre completo" class="form-control @error('full_name') is-invalid @enderror" name="name" value="{{ old('full_name') }}" required autocomplete="name" autofocus>
+            @error('full_name')
             <span class="text-danger">
                 <span>{{$message}}</span>
             </span>
             @enderror
-
         </div>
 
         <div class="input-content">
             <input type="text" name="email" placeholder="Correo eléctronico"
-                value="{{old('email')}}"
+                value="{{old('email')}}" 
                 autofocus>
-
-                @error('email')
+            @error('email')
             <span class="text-danger">
                 <span>{{$message}}</span>
             </span>
             @enderror
-
         </div>
 
         <div class="input-content">
@@ -56,4 +48,3 @@
     <p>¿Ya tienes una cuenta? <a href="{{route('login')}}" class="link">Iniciar sesión</a></p>
 </form>
 @endsection
-
