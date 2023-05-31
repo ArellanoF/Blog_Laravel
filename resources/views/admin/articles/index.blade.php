68fc07ad1b60eae581342f90713f353344db2193
ArellanoF <CORREO@example.comfrancescarellano@gmail.com>
2023-05-31 17:54:12 +0200

    list admin article

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content_header')
 <h2>Administra tus artículos</h2>
-@stop 
+@stop
 
 @section('content')
 
@@ -41,45 +41,41 @@
                     <td>{{ Str::limit($article->title, 25, '...') }}</td>
                     <td>{{ $article->category->name }}</td>
                     <td>
-                        <input type="checkbox" name="status" id="status" class="form-check-input ml-4"
-                        {{ $article->status ? 'checked=checked"' : '' }}
-                        disabled>
+                        <input type="checkbox" name="status" id="status" class="form-check-input ml-4" {{ $article->status ? 'checked=checked"' : '' }} disabled>
                     </td>
 
-                    <td width="2px"><a href="{{route('articles.show', $article->slug)}}"
-                            class="btn btn-primary btn-sm mb-2">Mostrar</a></td>
+                    <td width="2px"><a href="{{route('articles.show', $article->slug)}}" class="btn btn-primary btn-sm mb-2">Mostrar</a></td>
 
-                    <td width="5px"><a href="{{route('articles.edit', $article->slug)}}"
-                            class="btn btn-primary btn-sm mb-2">Editar</a></td>
+                    <td width="5px"><a href="{{route('articles.edit', $article->slug)}}" class="btn btn-primary btn-sm mb-2">Editar</a></td>
 
                     <td width="5px">
-                    <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirmDeleteModal">Eliminar</button>
-                    <div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Eliminación</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>¿Estás seguro de que deseas eliminar este artículo?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <form action="{{ route('articles.destroy', $article->slug) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Eliminar</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+                        <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirmDeleteModal">Eliminar</button>
+                        <div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Eliminación</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>¿Estás seguro de que deseas eliminar este artículo?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                        <form action="{{ route('articles.destroy', $article->slug) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Eliminar</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </td>
                 </tr>
-            @endforeach
+                @endforeach
             </tbody>
         </table>
         <div class="text-center mt-3">
